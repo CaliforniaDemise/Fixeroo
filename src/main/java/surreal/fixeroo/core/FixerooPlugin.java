cb2e815106eab53108b808dca0f955353305d589
CaliforniaDemise <vwwwwv@proton.me>
2024-10-13 23:45:09 +0300

    Fix RFG's random remapping errors with replacing it with Unimined.

--- src/main/java/surreal/fixeroo/core/FixerooPlugin.java
+++ src/main/java/surreal/fixeroo/core/FixerooPlugin.java
@@ -4,8 +4,8 @@ import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/surreal/fixeroo/events/EventXPOrb.java
+++ src/main/java/surreal/fixeroo/events/EventXPOrb.java
@@ -5,12 +5,11 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.registries.GameData;
-
-import javax.annotation.Nonnull;
+import org.jetbrains.annotations.NotNull;
 
 public class EventXPOrb {
     @SubscribeEvent
-    public static void remapLegacyClumps(@Nonnull RegistryEvent.MissingMappings<EntityEntry> event) {
+    public static void remapLegacyClumps(@NotNull RegistryEvent.MissingMappings<EntityEntry> event) {
         for(RegistryEvent.MissingMappings.Mapping<EntityEntry> mapping : event.getAllMappings()) {
             if(mapping.key.equals(new ResourceLocation("clumps", "xp_orb_big"))) {
                 mapping.remap(GameData.getEntityRegistry().getValue(2));
