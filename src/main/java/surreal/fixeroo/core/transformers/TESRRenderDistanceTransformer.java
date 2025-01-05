3e57755d01ed3cdb13c44abcd476268b05d5cee6
CaliforniaDemise <vwwwwv@proton.me>
2025-01-05 19:02:26 +0300

    Remove writeClass.

--- src/main/java/surreal/fixeroo/core/transformers/TESRRenderDistanceTransformer.java
+++ src/main/java/surreal/fixeroo/core/transformers/TESRRenderDistanceTransformer.java
@@ -19,7 +19,6 @@ public class TESRRenderDistanceTransformer extends TypicalTransformer {
                 break;
             }
         }
-        writeClass(cls);
         return write(cls);
     }
 
